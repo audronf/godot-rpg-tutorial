878085b972101a6dcddc0023a5a20659882f8602
audronf <federicofasitella@gmail.com>
2022-03-14 17:54:33 -0300

    Cleaned code by creating a constants file

---
+++ Constants.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const PLAYBACK_PARAMETER = "parameters/playback"
+const IDLE_BLEND_POSITION_PARAMETER = "parameters/Idle/blend_position"
+const RUN_BLEND_POSITION_PARAMETER = "parameters/Run/blend_position"
+const RUN = "Run"
+const IDLE = "Idle"
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500
+const ACCELERATION = 100
 const MAX_SPEED = 100
 const FRICTION = 500
 
@@ -8,7 +8,7 @@ var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
-onready var animationState = animationTree.get("parameters/playback")
+onready var animationState = animationTree.get(Constants.PLAYBACK_PARAMETER)
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -17,11 +17,11 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if (input_vector != Vector2.ZERO):
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationState.travel("Run")
+		animationTree.set(Constants.IDLE_BLEND_POSITION_PARAMETER, input_vector)
+		animationTree.set(Constants.RUN_BLEND_POSITION_PARAMETER, input_vector)
+		animationState.travel(Constants.RUN)
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		animationState.travel("Idle")
+		animationState.travel(Constants.IDLE)
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
